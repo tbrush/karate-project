b9f2e6e60da87dbcf2b56b0acac3e156f12f5ef8
Tim <gitbrush@gmail.com>
2017-07-10 18:18:57 -0500

    a
    a

--- group project/Form1.Designer.vb
+++ group project/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,23 +20,22 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.MembersBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        Me.Date_Selector = New System.Windows.Forms.DateTimePicker()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
         Me.IDDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.LastNameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.FirstNameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.PhoneDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.DateJoinedDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.MembersBindingSource = New System.Windows.Forms.BindingSource(Me.components)
         Me.KarateDataSetBindingSource = New System.Windows.Forms.BindingSource(Me.components)
         Me.KarateDataSet = New group_project.karateDataSet()
+        Me.Date_Selector = New System.Windows.Forms.DateTimePicker()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.MembersTableAdapter = New group_project.karateDataSetTableAdapters.MembersTableAdapter()
-        Me.Button1 = New System.Windows.Forms.Button()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.MembersBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.KarateDataSetBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -53,39 +52,6 @@ Partial Class Form1
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.Size = New System.Drawing.Size(543, 215)
         Me.DataGridView1.TabIndex = 0
-        '
-        'MembersBindingSource
-        '
-        Me.MembersBindingSource.DataMember = "Members"
-        Me.MembersBindingSource.DataSource = Me.KarateDataSetBindingSource
-        '
-        'Date_Selector
-        '
-        Me.Date_Selector.CustomFormat = "m/d/yyyy"
-        Me.Date_Selector.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
-        Me.Date_Selector.Location = New System.Drawing.Point(261, 12)
-        Me.Date_Selector.Name = "Date_Selector"
-        Me.Date_Selector.Size = New System.Drawing.Size(129, 20)
-        Me.Date_Selector.TabIndex = 1
-        Me.Date_Selector.Value = New Date(2017, 7, 7, 0, 0, 0, 0)
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(185, 19)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(70, 13)
-        Me.Label1.TabIndex = 2
-        Me.Label1.Text = "Select a date"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(27, 68)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(180, 13)
-        Me.Label2.TabIndex = 3
-        Me.Label2.Text = "Members who joined before this date"
         '
         'IDDataGridViewTextBoxColumn
         '
@@ -117,6 +83,11 @@ Partial Class Form1
         Me.DateJoinedDataGridViewTextBoxColumn.HeaderText = "Date_Joined"
         Me.DateJoinedDataGridViewTextBoxColumn.Name = "DateJoinedDataGridViewTextBoxColumn"
         '
+        'MembersBindingSource
+        '
+        Me.MembersBindingSource.DataMember = "Members"
+        Me.MembersBindingSource.DataSource = Me.KarateDataSetBindingSource
+        '
         'KarateDataSetBindingSource
         '
         Me.KarateDataSetBindingSource.DataSource = Me.KarateDataSet
@@ -127,25 +98,43 @@ Partial Class Form1
         Me.KarateDataSet.DataSetName = "karateDataSet"
         Me.KarateDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
+        'Date_Selector
+        '
+        Me.Date_Selector.CustomFormat = "m/d/yyyy"
+        Me.Date_Selector.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.Date_Selector.Location = New System.Drawing.Point(261, 12)
+        Me.Date_Selector.Name = "Date_Selector"
+        Me.Date_Selector.Size = New System.Drawing.Size(129, 20)
+        Me.Date_Selector.TabIndex = 1
+        Me.Date_Selector.Value = New Date(2017, 7, 7, 0, 0, 0, 0)
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(185, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Select a date"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(27, 68)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(180, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Members who joined before this date"
+        '
         'MembersTableAdapter
         '
         Me.MembersTableAdapter.ClearBeforeFill = True
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(50, 19)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 4
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(605, 321)
-        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Date_Selector)
@@ -174,5 +163,4 @@ Partial Class Form1
     Friend WithEvents FirstNameDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
     Friend WithEvents PhoneDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
     Friend WithEvents DateJoinedDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
-    Friend WithEvents Button1 As Button
 End Class
